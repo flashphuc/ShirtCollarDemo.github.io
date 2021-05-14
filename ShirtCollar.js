471932abbc95e999b42631efe80380a790860502
PhucPham <flashphuc219@gmail.com>
2021-05-14 16:08:25 +0700

    Update Code

---
+++ ShirtCollar.js
@@ -0,0 +1,61 @@
+
+function getEle(id) {
+    //Trả về đối tượng thẻ được tìm thấy
+    return document.getElementById(id);
+}
+// var div = getEle("change4")
+getEle("change4").addEventListener("mouseover", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/4.png')";
+}, false);
+getEle("change4").addEventListener("mouseout", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/4.png')";
+}, false);
+
+
+// var div = getEle("change4")
+getEle("change1").addEventListener("mouseover", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/1.png')";
+}, false);
+getEle("change1").addEventListener("mouseout", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/1.png')";
+}, false);
+
+// var div = getEle("change4")
+getEle("change2").addEventListener("mouseover", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/2.png')";
+}, false);
+getEle("change2").addEventListener("mouseout", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/2.png')";
+}, false);
+
+// var div = getEle("change4")
+getEle("change3").addEventListener("mouseover", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/3.png')";
+}, false);
+getEle("change3").addEventListener("mouseout", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/3.png')";
+}, false);
+
+// var div = getEle("change4")
+getEle("change5").addEventListener("mouseover", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/5.png')";
+}, false);
+getEle("change5").addEventListener("mouseout", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/5.png')";
+}, false);
+
+// var div = getEle("change4")
+getEle("change6").addEventListener("mouseover", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/6.png')";
+}, false);
+getEle("change6").addEventListener("mouseout", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/6.png')";
+}, false);
+
+// var div = getEle("change4")
+getEle("change7").addEventListener("mouseover", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/7.png')";
+}, false);
+getEle("change7").addEventListener("mouseout", function () {
+    getEle("picture__main").style.backgroundImage = "url('./img/7.png')";
+}, false);
