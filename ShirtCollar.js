0badf2b1a282a70d6aead4f8ac0c6ae0df1139b7
PhucPham <flashphuc219@gmail.com>
2021-05-15 11:16:38 +0700

    Update Code

--- ShirtCollar.js
+++ ShirtCollar.js
@@ -64,10 +64,10 @@ getEle("change7").addEventListener("mouseout", function () {
 
 // ShirtCollar White
 // var div = getEle("change4")
-getEle("change4__white").addEventListener("mouseover", function () {
+getEle("change1__white").addEventListener("mouseover", function () {
     getEle("picture__main__white").style.backgroundImage = "url('./img/White/1.png')";
 }, false);
-getEle("change4__white").addEventListener("mouseout", function () {
+getEle("change1__white").addEventListener("mouseout", function () {
     getEle("picture__main__white").style.backgroundImage = "url('./img/White/1.png')";
 }, false);
 
@@ -76,4 +76,39 @@ getEle("change3__white").addEventListener("mouseover", function () {
 }, false);
 getEle("change3__white").addEventListener("mouseout", function () {
     getEle("picture__main__white").style.backgroundImage = "url('./img/White/3.png')";
+}, false);
+
+getEle("change2__white").addEventListener("mouseover", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/2.png')";
+}, false);
+getEle("change2__white").addEventListener("mouseout", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/2.png')";
+}, false);
+
+getEle("change4__white").addEventListener("mouseover", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/4.png')";
+}, false);
+getEle("change4__white").addEventListener("mouseout", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/4.png')";
+}, false);
+
+getEle("change5__white").addEventListener("mouseover", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/5.png')";
+}, false);
+getEle("change5__white").addEventListener("mouseout", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/5.png')";
+}, false);
+
+getEle("change6__white").addEventListener("mouseover", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/6.png')";
+}, false);
+getEle("change6__white").addEventListener("mouseout", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/6.png')";
+}, false);
+
+getEle("change7__white").addEventListener("mouseover", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/7.png')";
+}, false);
+getEle("change7__white").addEventListener("mouseout", function () {
+    getEle("picture__main__white").style.backgroundImage = "url('./img/White/7.png')";
 }, false);
