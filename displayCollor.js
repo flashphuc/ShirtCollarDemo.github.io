ea348e3e1fa9f696e14c43a5b7910a2c90043fbc
PhucPham <flashphuc219@gmail.com>
2021-05-19 12:52:12 +0700

    Update Code

--- displayCollor.js
+++ displayCollor.js
@@ -6,16 +6,28 @@ getEle("changeDefault").addEventListener("click", function () {
     getEle("picture__main__whiteneck").style.display = "none";
     getEle("picture__main__white").style.display = "none";
     getEle("picture__main").style.display = "block";
+    getEle("changeDefault").style.opacity = "0.7"
+    getEle("changeWhite").style.opacity = "1";
+    getEle("changeWhiteNeck").style.opacity = "1"
 }, false);
 
 getEle("changeWhite").addEventListener("click", function () {
     getEle("picture__main").style.display = "none";
     getEle("picture__main__white").style.display = "block";
     getEle("picture__main__whiteneck").style.display = "none";
+    getEle("changeWhite").style.opacity = "0.7"
+    getEle("changeDefault").style.opacity = "1";
+    getEle("changeWhiteNeck").style.opacity = "1"
+
 }, false);
 
 getEle("changeWhiteNeck").addEventListener("click", function () {
     getEle("picture__main").style.display = "none";
     getEle("picture__main__white").style.display = "none";
     getEle("picture__main__whiteneck").style.display = "block";
+    getEle("changeWhiteNeck").style.opacity = "0.7";
+    getEle("changeDefault").style.opacity = "1";
+    getEle("changeWhite").style.opacity = "1"
+
+
 }, false);
